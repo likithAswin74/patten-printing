c591f094884466d994d1286a8f2969186a86c303
Likith Aswin <likithaswin14@gmail.com>
2025-04-11 12:37:55 +0530

    commited

---
+++ right_triangle.py
@@ -0,0 +1,16 @@
+n = 5 # int(input())
+
+for i in range(0, n):
+    for j in range(n-i-1):
+        print(" ", end="")
+
+    for k in range(i+1):
+        print("*", end="")
+    print()
+
+#op
+#     *
+#    **
+#   ***
+#  ****
+# *****
