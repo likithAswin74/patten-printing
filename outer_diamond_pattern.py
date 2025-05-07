8bcd577690fa6daac00946221d6a0e1a1a9ce7bc
Likith Aswin <likithaswin14@gmail.com>
2025-05-07 19:18:37 +0530

    first commit

---
+++ outer_diamond_pattern.py
@@ -0,0 +1,37 @@
+
+# outer diamond pattern
+n = 5
+
+for i in range(0, n):
+    for j in range(n-i-1):
+        print(" ", end="")
+
+    for k in range(i+1):
+        if k == 0 or k == i:
+            print("* ", end="")
+        else:
+            print("  ", end="")
+    print()
+
+for i in range(0, n):
+    for j in range(i+1):
+        print(" ", end="")
+
+    for k in range(n-i-1):
+        if k == 0 or k == n-i-2:
+            print("* ", end="")
+        else:
+            print("  ", end="")
+    print()
+#
+# # op
+#     *
+#    * *
+#   *   *
+#  *     *
+# *       *
+#  *     *
+#   *   *
+#    * *
+#     *
+
