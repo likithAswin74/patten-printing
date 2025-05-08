1ef7d56d8aa5dafb1e334bbec2004393ed98eb3c
Likith Aswin <likithaswin14@gmail.com>
2025-05-08 19:15:10 +0530

    first commit

---
+++ cross_pattern.py
@@ -0,0 +1,14 @@
+
+# for i in range(0, n):
+#     for j in range(i):
+#         print(" ", end="")
+#     print("* "*n, end="")
+#     print()
+#
+# op
+#
+# * * * * *
+#  * * * * *
+#   * * * * *
+#    * * * * *
+#     * * * * *
