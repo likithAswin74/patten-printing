a8c9af276a8a0610c9adcfbc7fea840e98e86b13
Likith Aswin <likithaswin14@gmail.com>
2025-05-12 20:17:16 +0530

    first commit

---
+++ 123pattern.py
@@ -0,0 +1,15 @@
+# 123 pattern
+# val = 1
+# for i in range(0, n):
+#     for j in range(i+1):
+#         print(val, end=" ")
+#         val += 1
+#     print()
+#
+# op
+#
+# 1
+# 2 3
+# 4 5 6
+# 7 8 9 10
+# 11 12 13 14 15
