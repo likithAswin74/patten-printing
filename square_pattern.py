a1a5a6f842f99a16f4b633515ca5d09534889147
Likith Aswin <likithaswin14@gmail.com>
2025-05-14 13:17:56 +0530

    first commit

--- square_pattern.py
+++ square_pattern.py
@@ -1,14 +1,15 @@
-# for i in range(0, n):
-#     for j in range(0, n):
-#         if i == 0 or j == 0 or i == n-1 or j == n-1:
-#             print("* ", end="")
-#         else:
-#             print("  ", end="")
-#     print()
-#
-#
+n = 5
+for i in range(0, n):
+    for j in range(0, n):
+        if i == 0 or j == 0 or i == n-1 or j == n-1:
+            print("* ", end="")
+        else:
+            print("  ", end="")
+    print()
+
+
 # op
-#
+
 # * * * * *
 # *       *
 # *       *
