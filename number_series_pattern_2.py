7a28c7ae43107570ef17b4a8b536a779db03cc18
Likith Aswin <likithaswin14@gmail.com>
2025-05-19 20:39:00 +0530

    changes comitted

--- number_series_pattern_2.py
+++ number_series_pattern_2.py
@@ -1,22 +1,22 @@
-# a = []
-# for i in range(n):
-#     for j in range(n-i-1):
-#         print("- ", end="")
-#     lst = []
-#     for k in range(i+1):
-#         if k == i:
-#             print("1", end="")
-#             lst.append(1)
-#         elif k == 0:
-#             print("1 * ", end="")
-#             lst.append(1)
-#         else:
-#             temp = a[k] + a[k-1]
-#             print(temp, end=" * ")
-#             lst.append(temp)
-#
-#     a = lst
-#     print()
+a = []
+for i in range(n):
+    for j in range(n-i-1):
+        print("- ", end="")
+    lst = []
+    for k in range(i+1):
+        if k == i:
+            print("1", end="")
+            lst.append(1)
+        elif k == 0:
+            print("1 * ", end="")
+            lst.append(1)
+        else:
+            temp = a[k] + a[k-1]
+            print(temp, end=" * ")
+            lst.append(temp)
+
+    a = lst
+    print()
 
 
 # op
