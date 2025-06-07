65e50dd3d32fc062e7c5d9d4910e32fb71157f80
Likith Aswin <likithaswin14@gmail.com>
2025-06-07 22:06:30 +0530

    first commit

---
+++ number_series_pattern_3.py
@@ -0,0 +1,21 @@
+n = 5
+# val = 0
+# for i in range(n):
+#     hold = 0
+#     for j in range(i+1):
+#         if j == 0:
+#             val += 1
+#             hold = val
+#             print(val, end=" ")
+#         else:
+#
+#             hold += (n-j)
+#             print(hold, end=" ")
+#     print()
+
+# op
+# 1
+# 2 6
+# 3 7 10
+# 4 8 11 13
+# 5 9 12 14 15
